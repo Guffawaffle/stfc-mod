c4ccc6f96f2bf78f122311966fde47e6bfcf071d
Guffawaffle <joeguff@gmail.com>
2026-06-21 23:53:52 -0500

    Fix mission marker fleet action travel

--- mods/src/prime/NavigationInteractionUIViewController.h
+++ mods/src/prime/NavigationInteractionUIViewController.h
@@ -2,6 +2,7 @@
 
 #include <il2cpp/il2cpp_helper.h>
 
+#include "MissionLocationWidget.h"
 #include "NavigationInteractionUIContext.h"
 #include "SetCourseWidget.h"
 #include "ViewController.h"
@@ -9,13 +10,22 @@
 struct NavigationInteractionUIViewController
     : public ViewController<NavigationInteractionUIContext, NavigationInteractionUIViewController> {
 public:
-  __declspec(property(get = __get__setCourseWidget)) SetCourseWidget* _setCourseWidget;
+  __declspec(property(get = __get__setCourseWidget)) SetCourseWidget*             _setCourseWidget;
+  __declspec(property(get = __get__missionLocationWidget)) MissionLocationWidget* _missionLocationWidget;
 
   void OnSetCourseButtonClick()
   {
     static auto OnSetCourseButtonClick =
         get_class_helper().GetMethod<void(NavigationInteractionUIViewController*)>("OnSetCourseButtonClick");
     OnSetCourseButtonClick(this);
+  }
+
+  void OnMissionLocationSetCourseButtonClick()
+  {
+    static auto OnMissionLocationSetCourseButtonClick =
+        get_class_helper().GetMethod<void(NavigationInteractionUIViewController*)>(
+            "OnMissionLocationSetCourseButtonClick");
+    OnMissionLocationSetCourseButtonClick(this);
   }
 
   static IL2CppClassHelper& get_class_helper()
@@ -29,6 +39,12 @@ public:
   {
     static auto field = get_class_helper().GetField("_setCourseWidget").offset();
     return *(SetCourseWidget**)((char*)this + field);
+  }
+
+  MissionLocationWidget* __get__missionLocationWidget()
+  {
+    static auto field = get_class_helper().GetField("_missionLocationWidget").offset();
+    return *(MissionLocationWidget**)((char*)this + field);
   }
 
 private:
