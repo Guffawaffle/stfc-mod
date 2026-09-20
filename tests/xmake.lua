be4e89771e562c8244f73ec94551500ecc23af1c
Guffawaffle <joeguff@gmail.com>
2026-09-20 14:12:36 -0500

    Preserve Unicode ship names and 64-bit locale IDs

--- tests/xmake.lua
+++ tests/xmake.lua
@@ -17,6 +17,18 @@ do
     add_includedirs("../mods/src")
 end
 
+target("ship-name-matching-tests")
+do
+    set_kind("binary")
+    set_default(false)
+    add_files("ship_name_matching_tests.cc")
+    add_includedirs("../mods/src")
+    add_packages("libil2cpp", "eastl", "spdlog", "simdutf")
+    if is_plat("windows") then
+        add_linkdirs("../mods/src/il2cpp")
+    end
+end
+
 target("shortcut-layout-dispatch-tests")
 do
     set_kind("binary")
