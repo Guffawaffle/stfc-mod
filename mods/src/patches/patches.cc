e27737d05ecf5243089f2cd290f26d76fe982b49
Guffawaffle <joeguff@gmail.com>
2026-09-25 20:39:00 -0500

    science: probe FT and CT equipment on ship card refresh

--- mods/src/patches/patches.cc
+++ mods/src/patches/patches.cc
@@ -58,6 +58,7 @@ void InstallOpcIndicatorHooks();
 #ifdef _MODDBG
 void InstallDevConsole();
 void InstallGameErrorProbe();
+void InstallShipTechProbe();
 #endif
 void InstallNativeSettings();
 void InstallGalaxyLabels();
@@ -204,6 +205,7 @@ __int64 il2cpp_init_hook(auto original, const char* domain_name)
 #ifdef _MODDBG
   InstallDevConsole();
   InstallGameErrorProbe();
+  InstallShipTechProbe();
 #endif
   InstallActionQueueRecovery();
   InstallThinQueueProtection();
